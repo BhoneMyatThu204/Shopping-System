afd53e0785c9331a7a76080552031c6075931147
BhoneMyatThu204 <ryder121203@gmail.com>
2026-08-22 10:57:05 +0630

    Add all project files

--- database.py
+++ database.py
@@ -4,22 +4,36 @@ def connect_db():
     conn = sqlite3.connect("shopping.db")
     cursor = conn.cursor()
     
+    # Products Table (UPDATED: Added 'image' column)
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS products (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
             price REAL NOT NULL,
-            stock INTEGER NOT NULL
+            stock INTEGER NOT NULL,
+            image TEXT NOT NULL
         )
     ''')
     
+    # Orders Table
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS orders (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             product_id INTEGER,
             quantity INTEGER,
             total_price REAL,
+            status TEXT DEFAULT 'Pending',
             FOREIGN KEY(product_id) REFERENCES products(id)
+        )
+    ''')
+
+    # Users Table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            role TEXT NOT NULL
         )
     ''')
     
